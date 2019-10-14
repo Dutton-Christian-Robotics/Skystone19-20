3291c20b3e4deaaa9df31cb385532af15f7b2791
Matthew Roberts <thew.roberts@gmail.com>
2019-10-14 10:14:16 -0400

    Initial version of testing configuration file for junior bot

---
+++ JuniorConfiguration.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+class JuniorConfiguration extends DefenderBotConfiguration {
+
+	public JuniorConfiguration() {
+		frontLeftMotorName 		= "Front Left Orange";
+		frontRightMotorName		= "Front Right Red";
+		rearLeftMotorName		= "Rear Left Camo";
+		rearRightMotorName		= "Rear Right Green";
+
+		forwardSecondsPerInch	= 0.0571895424836602;
+		sidewaysSecondsPerInch	= 0.0690476190476191;
+	}
+}
