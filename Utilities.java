0b80f276f5712ebd8c2ffdfe0a30e42d1ffa06ae
Matthew Roberts <thew.roberts@gmail.com>
2019-10-23 16:41:52 -0400

    Attempting to implement a timed loop that can be broken by boolean tripwires. Not yet used in robot code.

---
+++ Utilities.java
@@ -0,0 +1,94 @@
+//*******************************************************************
+// Welcome to CompileJava!
+// If you experience any issues, please contact us ('More Info')  -->
+//****************				***************************************************
+
+import java.lang.Math; // headers MUST be above the first class
+import java.util.concurrent.Callable;
+import java.util.*;
+
+// one class needs to have a main() method
+public class Main
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+    BreakableLoop loop = new BreakableLoop(new Callable<Void>() {
+      @Override
+      public Void call() {
+        System.out.println("Hello");
+        return null;
+      }
+    });
+    loop.addBreak(new CountToFive());
+
+    loop.run();
+  }
+}
+
+class BreakableLoop {
+  private ArrayList<BooleanTripwire> breaks = null;
+  private Callable codeBlock = null;
+
+  public BreakableLoop() {
+    breaks = new ArrayList<BooleanTripwire>();
+  }
+  public BreakableLoop(Callable<Void> myBlock) {
+    this();
+    codeBlock = myBlock;
+  }
+
+  public void addBreak(BooleanTripwire b) {
+   breaks.add(b);
+  }
+  public boolean anyTrue() {
+    boolean result = false;
+    Iterator<BooleanTripwire> it = breaks.iterator();
+    while (it.hasNext()) {
+      try {
+        result = result || it.next().call();
+      } catch (Exception e) {
+        result = true;
+        return result;
+      }
+    }
+    return result;
+  }
+
+  public void run() {
+    while (!anyTrue()) {
+      if (codeBlock != null) {
+        try {
+          codeBlock.call();
+        } catch (Exception e) {
+
+        }
+      } else {
+        System.out.println("Loop");
+      }
+    }
+
+  }
+}
+
+class BooleanTripwire implements Callable<Boolean> {
+
+  @Override
+  public Boolean call() throws Exception {
+   	return true;
+  }
+}
+
+class CountToFive extends BooleanTripwire {
+  private int count;
+
+  CountToFive() {
+    count = 0;
+  }
+
+  @Override
+  public Boolean call() throws Exception {
+    count += 1;
+    return count > 5;
+  }
+}
