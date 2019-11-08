354d52b629e7a33b3710546c5b9930587ef94900
Matthew Roberts <thew.roberts@gmail.com>
2019-11-08 07:18:27 -0500

    Created parent class for simple movement and manipulation.

---
+++ SimpleSettings.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+abstract public class SimpleSettings {
+
+	abstract public SimpleSettings clone();
+	abstract public SimpleSettings scale(double scalePercentage);
+	abstract public String toString();
+
+}
