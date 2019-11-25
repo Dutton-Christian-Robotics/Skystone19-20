7f5e4487830268b6f92b92cb0fd5606104ff011b
Matthew Roberts <thew.roberts@gmail.com>
2019-11-25 02:04:11 -0500

    Added additional manipulation and sensor based config options.

--- ProductionTestConfiguration.java
+++ ProductionTestConfiguration.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 class ProductionTestConfiguration extends DefenderBotConfiguration {
 
@@ -35,18 +36,34 @@ class ProductionTestConfiguration extends DefenderBotConfiguration {
 
 		liftMotorName = "LIFT MOTOR";
 		extendMotorName = "EXTEND MOTOR";
-		grabMotorName = "GRAB MOTOR";
+		grabMotorName = "CLAW MOTOR";
 		tiltMotorName = "TILT MOTOR";
+
+		grabMotorPower = 1;
+		capturePosition = 0;
+		releasePosition = 230;
+		resetPosition = 90;
+
 
 		liftMotorForwardDirection = DcMotor.Direction.REVERSE;
 		extendMotorForwardDirection = DcMotor.Direction.FORWARD;
 		grabMotorForwardDirection = DcMotor.Direction.REVERSE;
 		tiltMotorForwardDirection = DcMotor.Direction.FORWARD;
 
+		leftFoundationGrabberServoName = "GRABBER LEFT";
+		leftFoundationGrabberServoDirection = Servo.Direction.REVERSE;
+
+		rightFoundationGrabberServoName = "GRABBER RIGHT";
+		rightFoundationGrabberServoDirection = Servo.Direction.FORWARD;
+
+
+
 		forwardSecondsPerInch	= 0.05; //carpet at home
 		sidewaysSecondsPerInch	= 0.0690476190476191;
 
 		headingAxis = InternalGyroscopeService.Axis.Z;
+
+		vuforiaKey = "AUIxotb/////AAABmUiA7l9qRE16hqtSNI3A9PcEZRKBFEpztpXDtfcAsQJuv2fNHk4EHnNOpnt+hQVkB2gqjKMH7lBzvwPDrU4sQdXPO3HyuqKkA0QLoYP/QozKWgYx79lKOipOWtCs6RQGO8zPIWF12SqUrSf/zccfT9P4cwafrgHcxP/zlNJJUmSNCd3tcZxYKKOfvK2g97BmM2oad142iXfrvZPKIm96Re+krEVYy5cMb8pORKYzjFCrqfbFP8JarICUpI1plvrl5DwgWJGrwZLle/AEDuaCOSv5wWrbqHW7gq8SR9VMIG79eKZRXI1P6gEPMsxRiUchKwBwVeDXothphPoWtn1dNPLjg4lp2b4Xmok4bITUF+aA";
 
 	}
 }
