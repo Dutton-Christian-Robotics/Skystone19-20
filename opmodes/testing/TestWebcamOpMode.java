03162e8abc85c67350704a1ea3b647172214d5dd
Matthew Roberts <thew.roberts@gmail.com>
2019-11-27 08:41:16 -0500

    Created to allow testing of the Logitech webcam. Ended up being somewhat redundant.

---
+++ opmodes/testing/TestWebcamOpMode.java
@@ -0,0 +1,136 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import java.util.concurrent.Callable;
+import java.util.*;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Testing: Test Webcam", group="Iterative Opmode")
+
+public class TestWebcamOpMode extends OpMode {
+	private ElapsedTime runtime = new ElapsedTime();
+	private SkystoneBot bot = new SkystoneBot();
+	private static final float mmPerInch        = 25.4f;
+
+	@Override
+	public void init() {
+		telemetry.addData("Status", "Initializing");
+		telemetry.update();
+		try {
+			bot.init(hardwareMap, new ProductionTestConfiguration());
+		} catch (Exception e) {
+			telemetry.addData("bot init failed", "");
+			telemetry.update();
+
+		}
+
+		try {
+			int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+			bot.activateSkystoneVisionService(cameraMonitorViewId);
+		} catch (Exception e) {
+			telemetry.addData("failed vision service activate", "");
+			telemetry.update();
+
+		}
+
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+	@Override
+	public void start() {
+		runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        if (bot.skystoneVisionService.isSkystoneVisible()) {
+			telemetry.addData("Skystone", "yes");
+			telemetry.addData("foo", "%2d", bot.skystoneVisionService.foo());
+
+			telemetry.addData("avg Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+				bot.skystoneVisionService.dX, bot.skystoneVisionService.dY, bot.skystoneVisionService.dZ);
+
+			telemetry.addData("avg Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", bot.skystoneVisionService.rX, bot.skystoneVisionService.rY, bot.skystoneVisionService.rZ);
+
+                VectorF translation = bot.skystoneVisionService.rawDirectionToSkystone().getTranslation();
+                telemetry.addData("inst Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+
+                // express the rotation of the robot in degrees.
+                Orientation rotation = Orientation.getOrientation(bot.skystoneVisionService.rawDirectionToSkystone(), EXTRINSIC, XYZ, DEGREES);
+                telemetry.addData("inst Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
+
+
+
+        } else {
+	        telemetry.addData("Skystone", "no");
+        }
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+	    bot.shutdown();
+    }
+
+}
